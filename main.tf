ab142b3a0e8f975f38e6790ec3ad3f137db6ac7c
Kier Davis <me@kierdavis.com>
2025-07-24 20:11:34 +0100

    terraform fmt

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 variable "flake" {
-  type = string
+  type        = string
   description = <<-EOT
     A reference to an output of a Nix flake, in the form accepted by 'nix build'.
     For example, 'path:path/to/myflake#myoutput'.
@@ -17,7 +17,7 @@ variable "flake" {
 }
 
 variable "repo" {
-  type = string
+  type        = string
   description = <<-EOT
     The fully qualified name of the repository to push the built image to.
     For example, 'docker.io/kierdavis/myimage'.
@@ -40,20 +40,20 @@ data "external" "derivation" {
 }
 
 locals {
-  tag = data.external.derivation.result.tag
+  tag      = data.external.derivation.result.tag
   repo_tag = "${var.repo}:${local.tag}"
 }
 
 resource "terraform_data" "build_and_push" {
   triggers_replace = local.repo_tag
   provisioner "local-exec" {
-    command = "exec ${path.module}/tf-support/build-and-push.sh"
+    command     = "exec ${path.module}/tf-support/build-and-push.sh"
     environment = merge(data.external.derivation.result, { repo_tag = local.repo_tag })
   }
 }
 
 output "derivation" {
-  value = data.external.derivation.result.drv_path
+  value       = data.external.derivation.result.drv_path
   description = <<-EOT
     The Nix derivation for building the image, i.e. the result of evaluating
     the specified flake output. This is a filename of the form '/nix/store/*.drv'.
@@ -62,10 +62,10 @@ output "derivation" {
 
 output "tag" {
   depends_on = [terraform_data.build_and_push]
-  value = local.tag
+  value      = local.tag
 }
 
 output "repo_tag" {
   depends_on = [terraform_data.build_and_push]
-  value = local.repo_tag
+  value      = local.repo_tag
 }
